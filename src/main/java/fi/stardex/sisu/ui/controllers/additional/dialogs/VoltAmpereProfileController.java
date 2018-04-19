287323fc97fcc005de6f6c262b14428a0111c51f
nikolay <schaiydulin@outlook.com>
2018-04-19 10:44:07 +0300

    - VoltAmpereProfileController имеет связь с VoltageController

--- src/main/java/fi/stardex/sisu/ui/controllers/additional/dialogs/VoltAmpereProfileController.java
+++ src/main/java/fi/stardex/sisu/ui/controllers/additional/dialogs/VoltAmpereProfileController.java
@@ -1,11 +1,18 @@
 package fi.stardex.sisu.ui.controllers.additional.dialogs;
 
+import fi.stardex.sisu.ui.controllers.additional.tabs.VoltageController;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.ToggleButton;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
 
 public class VoltAmpereProfileController {
+
+    @Autowired
+    private VoltageController voltageController;
 
     @FXML
     private Spinner firstWSpinner;
@@ -39,4 +46,11 @@ public class VoltAmpereProfileController {
 
     @FXML
     private Button cancelBtn;
+
+    @PostConstruct
+    private void init() {
+        applyBtn.setOnMouseClicked(event -> {
+            System.err.println(voltageController);
+        });
+    }
 }
