9d566e9cc8bfe93f5e2ae96960bf8dd72746ccf0
nikolay <schaiydulin@outlook.com>
2018-05-18 17:54:51 +0300

    - Работа с лэдами. Настроен frequency spinner

---
+++ src/main/java/fi/stardex/sisu/listeners/FrequencySpinnerListener.java
@@ -0,0 +1,64 @@
+package fi.stardex.sisu.listeners;
+
+import fi.stardex.sisu.ui.controllers.cr.InjectorSectionController;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.Spinner;
+import javafx.util.StringConverter;
+
+import javax.annotation.PostConstruct;
+
+public class FrequencySpinnerListener implements ChangeListener<Double> {
+
+    private Spinner<Double> freqCurrentSignal;
+
+    private DoubleProperty spinnerValue = new SimpleDoubleProperty();
+
+    private StringConverter<Double> converter;
+
+    public FrequencySpinnerListener(InjectorSectionController injectorSectionController) {
+        freqCurrentSignal = injectorSectionController.getFreqCurrentSignal();
+        spinnerValue.bind(freqCurrentSignal.valueProperty());
+        converter = freqCurrentSignal.getValueFactory().getConverter();
+    }
+
+    @PostConstruct
+    public void init() {
+
+        // convert invalid text input back to double
+        freqCurrentSignal.getValueFactory().setConverter(new StringConverter<Double>() {
+
+            @Override
+            public String toString(Double object) {
+                return converter.toString(object);
+            }
+
+            @Override
+            public Double fromString(String string) {
+                try {
+                    return converter.fromString(string);
+                } catch(RuntimeException ex) {
+                    freqCurrentSignal.getValueFactory().setValue(16.67);
+                    return spinnerValue.get();
+                }
+            }
+        });
+
+        freqCurrentSignal.valueProperty().addListener(this);
+
+        // commit the value without pressing ENTER
+        freqCurrentSignal.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                freqCurrentSignal.increment(0);
+            }
+        });
+    }
+
+
+    @Override
+    public void changed(ObservableValue<? extends Double> observable, Double oldValue, Double newValue) {
+        //TODO:
+    }
+}
