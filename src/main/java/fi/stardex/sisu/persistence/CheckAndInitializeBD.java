38cddb71d1ecb1877e3e360c0d313a45508f401f
nikolay <schaiydulin@outlook.com>
2018-07-19 17:40:57 +0300

    - Тестирование сохранения бд у пользователей. Update2

--- src/main/java/fi/stardex/sisu/persistence/CheckAndInitializeBD.java
+++ src/main/java/fi/stardex/sisu/persistence/CheckAndInitializeBD.java
@@ -11,6 +11,8 @@ import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
 import java.io.*;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 @PropertySource("classpath:properties/app.properties")
 public class CheckAndInitializeBD {
@@ -19,14 +21,39 @@ public class CheckAndInitializeBD {
     private ManufacturerRepository manufacturerRepository;
     private DataSource dataSource;
 
-    @Value("${stardex.version.directory}")
-    private String versionDirectory;
+    private static final String COMMA_DELIMITER = ",";
+
+    private static final String NEW_LINE_SEPARATOR = "\n";
+
+    @Value("${stardex.custom_csvs.directory}")
+    private String customCSVSDirectory;
+
+    @Value("${stardex.directory}")
+    private String directory;
 
     @Value("${stardex.version.file.name}")
     private String versionFileName;
 
     @Value("${stardex.version}")
     private String version;
+
+    @Value("${stardex.custom_csvs.manufacturers}")
+    private String customManufacturers;
+
+    @Value("${stardex.custom_csvs.injectorTypes}")
+    private String customInjectorTypes;
+
+    @Value("${stardex.custom_csvs.voltAmpereProfiles}")
+    private String customVOAP;
+
+    @Value("${stardex.custom_csvs.injectors}")
+    private String customInjectors;
+
+    @Value("${stardex.custom_csvs.testNames}")
+    private String customTestNames;
+
+    @Value("${stardex.custom_csvs.injectorTests}")
+    private String customInjectorTests;
 
     public CheckAndInitializeBD(ManufacturerRepository manufacturerRepository, DataSource dataSource) {
         this.manufacturerRepository = manufacturerRepository;
@@ -38,7 +65,7 @@ public class CheckAndInitializeBD {
     private void check() {
 
         try {
-            if (isCurrentVersion()) {
+            if (!isCurrentVersion()) {
                 updateDB();
             } else {
                 if (manufacturerRepository.count() == 0) {
@@ -54,11 +81,45 @@ public class CheckAndInitializeBD {
             }
         }
 
-//        if (manufacturerRepository.count() == 0) {
-//            fillTables();
-//        } else {
-//            System.err.println("Tables filled out");
-//        }
+    }
+
+    private void checkCustomCSVS() {
+
+        File fileDir = new File(System.getProperty("user.home") + File.separator + directory + File.separator + customCSVSDirectory);
+
+        fileDir.mkdirs();
+
+        File customManufacturersFile = new File(fileDir, customManufacturers);
+
+        File customInjectorTypesFile = new File(fileDir, customInjectorTypes);
+
+        File customVOAPFile = new File(fileDir, customVOAP);
+
+        File customInjectorsFile = new File(fileDir, customInjectors);
+
+        File customTestNamesFile = new File(fileDir, customTestNames);
+
+        File customInjectorTestsFile = new File(fileDir, customInjectorTests);
+
+//        List<File> listOfCustomCSVSFiles = new ArrayList<>();
+//
+//        listOfCustomCSVSFiles.add(customManufacturersFile);
+//
+//        listOfCustomCSVSFiles.add(customInjectorTypesFile);
+//
+//        listOfCustomCSVSFiles.add(customVOAPFile);
+//
+//        listOfCustomCSVSFiles.add(customInjectorsFile);
+//
+//        listOfCustomCSVSFiles.add(customTestNamesFile);
+//
+//        listOfCustomCSVSFiles.add(customInjectorTestsFile);
+//
+//        listOfCustomCSVSFiles.forEach(file -> {
+//            if (!file.exists() || !file.isFile()) {
+//
+//            }
+//        });
 
     }
 
@@ -66,7 +127,7 @@ public class CheckAndInitializeBD {
 
         boolean isCurrent;
 
-        File fileDir = new File(System.getProperty("user.home") + File.separator + versionDirectory);
+        File fileDir = new File(System.getProperty("user.home") + File.separator + directory);
 
         fileDir.mkdirs();
 
@@ -97,8 +158,19 @@ public class CheckAndInitializeBD {
     }
 
     private void updateDB() {
-        dropTables();
+        deleteTables();
         fillTables();
+    }
+
+    private void deleteTables() {
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/db/scripts/delete_tables.sql")));
+            RunScript.execute(dataSource.getConnection(), reader);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void fillTables() {
@@ -109,20 +181,7 @@ public class CheckAndInitializeBD {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-//        String filePath = getClass().getResource("/db/csv/manufacturers.csv").getPath();
-//        sessionFactory.openSession().createQuery("INSERT INTO MANUFACTURER(ID, MANUFACTURER, DISPLAY_ORDER) SELECT manufacturer FROM CSVREADER('"+
-//               filePath +"')");
 
-
-    }
-
-    private void dropTables() {
-        try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(getClass().getResourceAsStream("/db/scripts/drop_tables.sql")));
-            RunScript.execute(dataSource.getConnection(), reader);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 
 }
