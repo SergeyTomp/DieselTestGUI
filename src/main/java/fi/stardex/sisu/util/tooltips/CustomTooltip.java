4476550f427fd601003ffeb4ece2baefc2411ecb
Sergey_Tomp <tsv.091265@gmail.com>
2018-11-09 12:55:24 +0300

    Migration_to Java11

--- src/main/java/fi/stardex/sisu/util/tooltips/CustomTooltip.java
+++ src/main/java/fi/stardex/sisu/util/tooltips/CustomTooltip.java
@@ -21,16 +21,16 @@ public class CustomTooltip extends Tooltip {
 
     private void hackTooltipStartTiming() {
         try {
-            Field fieldBehavior = Tooltip.class.getDeclaredField("BEHAVIOR");
-            fieldBehavior.setAccessible(true);
-            Object objBehavior = fieldBehavior.get(this);
-
-            Field fieldTimer = objBehavior.getClass().getDeclaredField("activationTimer");
-            fieldTimer.setAccessible(true);
-            Timeline objTimer = (Timeline) fieldTimer.get(objBehavior);
-
-            objTimer.getKeyFrames().clear();
-            objTimer.getKeyFrames().add(new KeyFrame(new Duration(250)));
+//            Field fieldBehavior = Tooltip.class.getDeclaredField("BEHAVIOR");
+//            fieldBehavior.setAccessible(true);
+//            Object objBehavior = fieldBehavior.get(this);
+//
+//            Field fieldTimer = objBehavior.getClass().getDeclaredField("activationTimer");
+//            fieldTimer.setAccessible(true);
+//            Timeline objTimer = (Timeline) fieldTimer.get(objBehavior);
+//
+//            objTimer.getKeyFrames().clear();
+//            objTimer.getKeyFrames().add(new KeyFrame(new Duration(250)));
         } catch (Exception e) {
             e.printStackTrace();
         }
