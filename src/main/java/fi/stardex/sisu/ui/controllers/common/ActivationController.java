3de2cb4c2b239600ee3655fdaf22476ce8a001c6
Sergey_Tomp <tsv.091265@gmail.com>
2020-09-04 10:54:50 +0300

    Доработка активации при лизинге, пока деактивирована для основного релиза. Completed 04.09.2020 11.00
    - в ActivationController:
     -- изменена периодическая отправка текущего времени в железо - вместо цикла в отдельном потоке теперь Timeline, чтобы   остановить при потере коннекта и перезапустить по установлению.
     -- листенер ошибки активации вынесен в отдельную переменную activationErrorListener чтобы можно было его навешивать по установлению или удалять при потере коннекта
     -- добавлена проверка валидности строки кода перед отправкой вниз на предмет разрешённых символов и количества, если не проходит - выводится сообщение о неправильности кода без отправки
     -- обнуление регистра Activation_errorCode по просьбе Дениса перенесено в начало обработчика кнопки applyButton вместо activationErrorListener
    
    Деактивация сделана установкой переменной autoUpdate для Activation_error в false и комментированием ultimaModbusConnect.connectedProperty().addListener.....
    
    Проверить: нужно ли в ActivationController оставить number = number & 0xFFFF перед преобразованием в строку;

--- src/main/java/fi/stardex/sisu/ui/controllers/common/ActivationController.java
+++ src/main/java/fi/stardex/sisu/ui/controllers/common/ActivationController.java
@@ -10,6 +10,7 @@ import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
@@ -29,9 +30,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 import static fi.stardex.sisu.registers.ultima.ModbusMapUltima.*;
+import static javafx.animation.Animation.INDEFINITE;
 
 public class ActivationController {
 
@@ -62,6 +65,9 @@ public class ActivationController {
     private final StringProperty codeAccepted = new SimpleStringProperty();
     private final StringProperty finalCodeAccepted = new SimpleStringProperty();
     private Timeline closeDialog;
+    private Timeline timeUpdate;
+    private ChangeListener<Boolean> activationErrorListener;
+    private Pattern pattern;
 
     public void setDialogViev(Parent dialogViev) {
         this.dialogViev = dialogViev;
@@ -85,17 +91,61 @@ public class ActivationController {
 
         ultimaRegisterProvider = ultimaModbusWriter.getRegisterProvider();
         acceptanceLabel.setText("");
+        pattern = Pattern.compile("[A-Fa-f0-9]*");
+
         closeDialog = new Timeline(new KeyFrame(Duration.millis(3000), actionEvent -> dialogStage.close()));
         closeDialog.setCycleCount(1);
 
+        timeUpdate = new Timeline(new KeyFrame(Duration.millis(60000), actionEvent -> sendTime()));
+        timeUpdate.setCycleCount(INDEFINITE);
+
+        activationErrorListener = (observableValue, oldValue, newValue) -> {
+            if (newValue) {
+                try {
+                    Integer[] payment = ultimaRegisterProvider.readBytePacket(Activation_paymentKey.getRef(), Activation_paymentKey.getCount());
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int number : payment){
+
+                        number = number & 0xFFFF;
+                        if (number < 0xF) {
+                            sb.append("000");
+                        }
+                        else if (number < 0xFF) {
+                            sb.append("00");
+                        }
+                        else if(number < 0xFFF){
+                            sb.append("0");
+                        }
+                        sb.append(Integer.toHexString(number));
+                    }
+                    paymentCodeTextField.setText(swapPairs(sb.toString().toUpperCase()));
+                    activationTextField.clear();
+                    showDialog();
+                } catch (ModbusException e) {
+                    logger.error("Activation Code reading failed!");
+                    e.printStackTrace();
+                }
+            }
+        };
+
         applyButton.setOnMouseClicked(mouseEvent -> {
+
+            ultimaModbusWriter.add(Activation_errorCode, 0);
+            acceptanceLabel.setText("");
 
             if (requestTask != null && requestTask.isRunning()) {
                 requestTask.cancel(true);
                 logger.warn("Activation code check cancelled!");
             }
 
-            ultimaModbusWriter.add(Activation_key, swapPairs(activationTextField.getText().toLowerCase()));
+            String codeString = activationTextField.getText().toLowerCase();
+            if (codeString.length() != CODE_SIZE || !pattern.matcher(codeString).matches()) {
+                acceptanceLabel.setText(incorrectCode.get());
+                return;
+            }
+
+            ultimaModbusWriter.add(Activation_key, swapPairs(codeString));
 
             requestTask = new Task<>() {
 
@@ -109,9 +159,8 @@ public class ActivationController {
                             Thread.sleep(500);
                         }
                     }catch (InterruptedException e){
+                        reply = -1;
                         logger.error("Activation code check interrupted!");
-                        e.printStackTrace();
-                        return -1;
                     }
                     if (isCancelled()) {
                         return -1;
@@ -146,7 +195,6 @@ public class ActivationController {
 
         cancelButton.setOnMouseClicked(mouseEvent -> Platform.exit());
 
-
 //        ultimaModbusConnect.connectedProperty().addListener((observableValue, oldValue, newValue) -> {
 //
 //            if (newValue) {
@@ -156,52 +204,18 @@ public class ActivationController {
 //                Object lastValue_1 = Version_controllable_1.getLastValue();
 //                Object lastValue_2 = Version_controllable_2.getLastValue();
 //
-//                if ((lastValue_1 != null && lastValue_2 != null && (int)lastValue_1 != 0xF1 && (int)lastValue_2 != 0xAA)) return;
-//                if (((int)Main_version_0.getLastValue()) / 10  != 4) return;
+//                if ((lastValue_1 != null && lastValue_2 != null && (int) lastValue_1 != 0xF1 && (int) lastValue_2 != 0xAA)) return;
+//                if (((int) Main_version_0.getLastValue()) / 10 != 4) return;
 //
-//                new Thread(() -> {
-//                    while (!Thread.currentThread().isInterrupted()) {
-//                        sendTime();
-//                        try {
-//                            Thread.sleep(600000);
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }).start();
+//                injectorSectionUpdateModel.activationErrorProperty().setValue(false);
+//                injectorSectionUpdateModel.activationErrorProperty().addListener(activationErrorListener);
+//                sendTime();
+//                timeUpdate.play();
+//            } else {
+//                injectorSectionUpdateModel.activationErrorProperty().removeListener(activationErrorListener);
+//                timeUpdate.stop();
 //            }
 //        });
-
-        injectorSectionUpdateModel.activationErrorProperty().addListener((observableValue, oldValue, newValue) -> {
-            if (newValue) {
-                try {
-                    Integer[] payment = ultimaRegisterProvider.readBytePacket(Activation_paymentKey.getRef(), Activation_paymentKey.getCount());
-                    StringBuilder sb = new StringBuilder();
-
-                    for (int number : payment){
-
-                        number = number & 0xFFFF;
-                        if (number < 0xF) {
-                            sb.append("000");
-                        }
-                        else if (number < 0xFF) {
-                            sb.append("00");
-                        }
-                        else if(number < 0xFFF){
-                            sb.append("0");
-                        }
-                        sb.append(Integer.toHexString(number));
-                    }
-                    paymentCodeTextField.setText(swapPairs(sb.toString().toUpperCase()));
-                    activationTextField.clear();
-                    ultimaModbusWriter.add(Activation_errorCode, 0);
-                    showDialog();
-                } catch (ModbusException e) {
-                    logger.error("Activation Code reading failed!");
-                    e.printStackTrace();
-                }
-            }
-        });
 
         bindingI18N();
     }
